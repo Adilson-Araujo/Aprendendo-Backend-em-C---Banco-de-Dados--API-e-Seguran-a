c3334b76ba45efdadf87ba873add750e09da4af4
adilson.araujo <adilson.araujo@bureauveritas.com>
2026-09-02 15:23:53 -0300

    Banco de Dados

--- CadastroProdutos/Program.cs
+++ CadastroProdutos/Program.cs
@@ -1,4 +1,6 @@
+using CadastroProdutos.Database;
 using CadastroProdutos.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,8 @@ builder.Services.AddSwaggerGen(); // Swagger
 
 // Injeção de Dependência
 builder.Services.AddScoped<IProdutosService, ProdutosService>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source = Produtos.db"));
 
 var app = builder.Build();
 app.MapControllers(); // Controllers
